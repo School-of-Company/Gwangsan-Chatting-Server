2c215569c51cc4cd9407b50af53aec4f552241a7
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-20 15:49:16 +0900

    add :: chat-message-response.dto 추가

---
+++ src/chat/dto/chat-message-response.dto.ts
@@ -0,0 +1,17 @@
+import { ChatImageResponse } from "./chat-image-response.dto";
+import { MessageType } from "./constant/message-type.enum";
+
+export class ChatmessageResponseDto {
+    constructor(
+        public readonly messageId: number,
+        public readonly roomId: number,
+        public readonly content: string | null,
+        public readonly messageType: MessageType,
+        public readonly createdAt: Date,
+        public readonly images: ChatImageResponse[] | null,
+        public readonly senderNickname: string,
+        public readonly senderId: number,
+        public readonly checked: boolean,
+        public isMine: boolean
+    ) {}
+}
