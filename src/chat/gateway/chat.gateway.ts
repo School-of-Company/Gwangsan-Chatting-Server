6371b77c81851e2d0004c6bbfe9018c47cd72931
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-20 15:50:07 +0900

    add :: chat.gateway 추가

---
+++ src/chat/gateway/chat.gateway.ts
@@ -0,0 +1,83 @@
+import { Inject } from '@nestjs/common';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsException } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { ChatMessageRequest } from '../dto/chat-message-request.dto';
+import { ChatmessageResponseDto } from '../dto/chat-message-response.dto';
+import { memberInfo } from '../dto/chat-member-info.dto';
+import { IAUTH_TOKEN_SERVICE, ISEND_CHAT_MESSAGE_SERVICE } from 'src/core/di.tokens';
+import { ISendChatMessageService } from '../service/isend-chat-message.interface';
+import { IAuthTokenService } from '../service/iauth-token.service';
+
+@WebSocketGateway({cors: true})
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+
+  constructor(
+    @Inject(ISEND_CHAT_MESSAGE_SERVICE) private readonly sendChatMessageService: ISendChatMessageService,
+    @Inject(IAUTH_TOKEN_SERVICE) private readonly authTokenService: IAuthTokenService,
+  ) {}
+
+  afterInit(server: Server) {
+    this.server = server;
+    console.log('Socket server initialized');
+  }
+
+  async handleConnection(client: Socket): Promise<void> {
+    try {
+      const token = client.handshake.auth.token;
+
+      if (!token) {
+        console.log(`Clinet disconnected: ${client.id}`);
+        client.disconnect();
+        return;
+      }
+
+      const memberInfo: memberInfo = await this.authTokenService.execute(token);
+
+      client.data.memberId = memberInfo.memberId;
+      client.data.nickname = memberInfo.nickname;
+    } catch (error) {
+      client.disconnect();
+    }
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Clinet disconnected: ${client.id}`);
+    client.disconnect();
+  }
+
+  @SubscribeMessage('sendMessage')
+  async handleMessage(
+    @MessageBody() message: ChatMessageRequest,
+    @ConnectedSocket() client: Socket,
+  ): Promise<void> {
+    const token = client.handshake.auth.token;
+
+    if (!token) {
+      throw new WsException('Authorization header missing');
+    }
+
+    client.join(`roomId=${message.roomId}`);
+
+    const response = await this.sendChatMessageService.execute(message, client, token);
+
+    const sockets = await this.server.in(`roomId=${message.roomId}`).fetchSockets();
+
+    for (const socket of sockets) {
+      const customizedResponse = new ChatmessageResponseDto(
+        response.messageId,
+        response.roomId,
+        response.content,
+        response.messageType,
+        response.createdAt,
+        response.images,
+        response.senderNickname,
+        response.senderId,
+        response.checked,
+        socket.data.memberId === response.senderId
+      );
+  
+      socket.emit('receiveMessage', customizedResponse);
+    }
+  }
+}
