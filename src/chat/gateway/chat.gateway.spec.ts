fb3a98db70d03bf7a170c6b60e449798e65f9861
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-20 16:06:54 +0900

    add :: 의존성 추가

--- src/chat/gateway/chat.gateway.spec.ts
+++ src/chat/gateway/chat.gateway.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatGateway } from './chat.gateway';
+import { ISEND_CHAT_MESSAGE_SERVICE, IAUTH_TOKEN_SERVICE } from 'src/core/di.tokens';
+
+const mockSendChatMessageService = {
+  execute: jest.fn(),
+  notifyReadStatusToSpring: jest.fn(),
+};
+
+const mockAuthTokenService = {
+  execute: jest.fn(),
+};
 
 describe('ChatGateway', () => {
   let gateway: ChatGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChatGateway],
+      providers: [
+        ChatGateway,
+        {
+          provide: ISEND_CHAT_MESSAGE_SERVICE,
+          useValue: mockSendChatMessageService,
+        },
+        {
+          provide: IAUTH_TOKEN_SERVICE,
+          useValue: mockAuthTokenService,
+        },
+      ],
     }).compile();
 
     gateway = module.get<ChatGateway>(ChatGateway);
