8054e707e880ba8a78f3050222993d355e09b5f5
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-21 09:43:34 +0900

    update :: 예외 처리 추가

--- src/domain/chat/service/impl/auth-token.service.ts
+++ src/domain/chat/service/impl/auth-token.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, UnauthorizedException } from "@nestjs/common";
 import { IAuthTokenService } from "../iauth-token.service";
 import { memberInfo } from "src/domain/chat/dto/chat-member-info.dto";
 import axios from "axios";
@@ -17,7 +17,14 @@ export class AuthTokenService implements IAuthTokenService {
 
             return response.data;
         } catch (error) {
-            throw error;
+            if (axios.isAxiosError(error)) {
+                if (error.response?.status === 401) {
+                    throw new UnauthorizedException('인증에 실패했습니다.');
+                }
+                // 그 외 서버 에러
+                throw new InternalServerErrorException('서버 내부 오류가 발생했습니다.');
+            }
+            throw error; // axios가 아닌 다른 에러면 그냥 던짐
         }
     }
 }
